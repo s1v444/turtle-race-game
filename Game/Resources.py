b920f2fe5ce1f5a11f12cfad4cf8b3928ff908a5
s1v444 <s61062578@gmail.com>
2025-05-05 23:33:16 +0300

    Update Resources.py

--- Game/Resources.py
+++ Game/Resources.py
@@ -1,17 +1,17 @@
 import pygame
 from Constants import WIDTH, HEIGHT
 
-#Функция для обработки изображения
+# Функция для обработки изображения
 def load_images():
-    red_turtle_img = pygame.image.load('images/red.jpg')
-    blue_turtle_img = pygame.image.load('images/blue.jpg')
-    yellow_turtle_img = pygame.image.load('images/yellow.jpg')
+    red_turtle_img = pygame.image.load('images/red.png').convert_alpha()
+    blue_turtle_img = pygame.image.load('images/blue.png').convert_alpha()
+    yellow_turtle_img = pygame.image.load('images/yellow.png').convert_alpha()
     sea_img = pygame.image.load('images/sea.jpg')
 
-    turtle_size = (50, 50) #Аргумент изображение, кортеж
+    turtle_size = (50, 50)
     red_turtle_img = pygame.transform.scale(red_turtle_img, turtle_size)
     blue_turtle_img = pygame.transform.scale(blue_turtle_img, turtle_size)
     yellow_turtle_img = pygame.transform.scale(yellow_turtle_img, turtle_size)
     sea_img = pygame.transform.scale(sea_img, (WIDTH, HEIGHT))
 
-    return red_turtle_img, blue_turtle_img, yellow_turtle_img, sea_img #Возвращает готовых черепашек для игры
+    return red_turtle_img, blue_turtle_img, yellow_turtle_img, sea_img
